8909132b2569343451dcce170ca3765a8367e96d
angelo <rxhuang@live.ca>
2021-05-05 16:50:46 -0700

    First commit

--- Project4_3/DataProducer/src/main/java/DataProducer.java
+++ Project4_3/DataProducer/src/main/java/DataProducer.java
@@ -2,7 +2,7 @@ import org.apache.kafka.clients.producer.Producer;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import org.json.simple.JSONObject;
+import org.json.JSONObject;
 
 public class DataProducer {
     private Producer<String, String> producer;
