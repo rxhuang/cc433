c419a165fa1bcff2c6378f5993341fb0a9dcb200
angelo <rxhuang@live.ca>
2021-05-05 17:25:10 -0700

    First commit

--- Project4_3/DataProducer/src/main/java/DataProducer.java
+++ Project4_3/DataProducer/src/main/java/DataProducer.java
@@ -33,7 +33,7 @@ public class DataProducer {
         
         
             JSONObject lineJson = new JSONObject(strCurrentLine);
-            String blockID = lineJson.getInt("blockId");
+            int blockID = lineJson.getInt("blockId");
 
             if(lineJson.getString("type").equals("DRIVER_LOCATION")){
                 this.producer.send(new ProducerRecord<String, String>("driver-locations", blockID, null, strCurrentLine));
