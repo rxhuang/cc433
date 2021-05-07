ed095a5e559abeea18020adafcf56f00091223a8
angelo <rxhuang@live.ca>
2021-05-07 12:00:10 -0700

    First commit

--- driver-match/driver-match/src/main/java/com/cloudcomputing/samza/nycabs/DriverMatchTask.java
+++ driver-match/driver-match/src/main/java/com/cloudcomputing/samza/nycabs/DriverMatchTask.java
@@ -143,10 +143,11 @@ public class DriverMatchTask implements StreamTask, InitableTask {
                         Entry<String, String> entry = entries.next();
                         int driverId = Integer.valueOf(entry.getKey().split(",")[1]);
                         JSONObject driver = new JSONObject(entry.getValue());
+                        String status = "";
                         if (driver.has("status")) {
-                            String status = driver.getString("status");
+                            status = driver.getString("status");
                         } else {
-                            String status = "UNAVAILABLE";
+                            status = "UNAVAILABLE";
                         }
 
 
