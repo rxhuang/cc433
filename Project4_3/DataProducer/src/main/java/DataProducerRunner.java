ad52534a1ca93a9f78670d87dbebd1c6585d9877
angelo <rxhuang@live.ca>
2021-05-06 22:56:50 -0700

    First commit

--- Project4_3/DataProducer/src/main/java/DataProducerRunner.java
+++ Project4_3/DataProducer/src/main/java/DataProducerRunner.java
@@ -16,7 +16,7 @@ public class DataProducerRunner {
         */
         
         Properties props = new Properties();
-        props.put("bootstrap.servers", "172.31.42.137:9092,172.31.33.102:9092,172.31.33.41:9092");
+        props.put("bootstrap.servers", "172.31.34.131:9092,172.31.46.134:9092,172.31.32.198:9092");
         props.put("acks", "all");
         props.put("retries", 0);
         props.put("batch.size", 16384);
