b30a2c98f39d016a5970a746167c57331bbdfea9
angelo <rxhuang@live.ca>
2021-05-07 00:07:02 -0700

    First commit

--- Project4_3/DataProducer/src/main/java/DataProducerRunner.java
+++ Project4_3/DataProducer/src/main/java/DataProducerRunner.java
@@ -19,7 +19,7 @@ public class DataProducerRunner {
         props.put("bootstrap.servers", "172.31.34.131:9092,172.31.46.134:9092,172.31.32.198:9092");
         props.put("acks", "all");
         props.put("retries", 0);
-        props.put("batch.size", 16384);
+        props.put("batch.size", 200);
         props.put("linger.ms", 1);
         props.put("buffer.memory", 33554432);
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
